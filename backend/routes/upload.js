0a135bdbadf2cfae0c291f5ae1c78e5f99ea4b63
duyunfeng <944588913@qq.com>
2025-05-26 12:31:10 +0800

    feat:修改文件上传

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -1,10 +1,27 @@
 import { Router } from 'express'
 import multer from 'multer'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { v4 as uuidv4 } from 'uuid'
 import * as uploadController from '../controllers/upload.js'
 
 const router = Router()
 
-const upload = multer({ dest: 'uploads/' })
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, '../../public/uploads'))
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = uuidv4()
+    const extension = path.extname(file.originalname)
+    cb(null, uniqueSuffix + extension)
+  },
+})
+
+const upload = multer({ storage: storage })
 
 router.post('/', upload.single('file'), uploadController.uploadFile)
 
